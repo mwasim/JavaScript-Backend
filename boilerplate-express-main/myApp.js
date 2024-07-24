5c67ef701a7969602d179b0228ddb0ae621b781e
Wasim <mwasimse@gmail.com>
2024-07-24 13:35:39 +0500

    Node and Express Demo - serve and HTML file, and use middleware to serve static assets like styesheets

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,39 +1,19 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+const path = require("path");
 
+//console.log("Hello!", path.join(__dirname, "views", "index.html"));
 
+//middleware to server static assets
+app.use("/public", express.static(path.join(__dirname, "public")));
 
+//Serve an HTML file
+app.get("/home", (req, res) => {
+  //const path = `${__dirname}/views/index.html`;
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
