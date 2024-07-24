cdc9c4c942c3c3526372f807fe98a574b4ae29c4
Wasim <mwasimse@gmail.com>
2024-07-24 13:37:35 +0500

    Node and Express Demo - serve JSON by reading environment variables from .evn file.

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -13,6 +13,19 @@ app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+//Serve jSON on a specific route
+app.get("/json", (req, res) => {
+  const messageText = "Hello json";
+
+  //read environment variables (setup in .env)
+  let message =
+    process.env.MESSAGE_STYLE === "uppercase"
+      ? messageText.toUpperCase()
+      : messageText;
+  res.json({ message: message });
+});
+
+//list server on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
