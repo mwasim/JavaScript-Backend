420424e5811759cd10ea521ede5c69b20aa8b774
Wasim <mwasimse@gmail.com>
2024-08-04 15:12:24 +0500

    Timestamp Microservice API project completed

---
+++ timestamp-microservice/index.js
@@ -0,0 +1,47 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your API endpoint for timestamp microservice
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+  let dateObj;
+
+  if (!dateParam) {
+    dateObj = new Date();
+  } else if (!isNaN(dateParam)) {
+    dateObj = new Date(parseInt(dateParam));
+  } else {
+    dateObj = new Date(dateParam);
+  }
+
+  if (isNaN(dateObj.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: dateObj.getTime(),
+      utc: dateObj.toUTCString(),
+    });
+  }
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
